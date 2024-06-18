16dd6a99bc83980ecf5fc04eeac74d74fbfcf81c
myduckperson <illarionov@gmail.com>
2024-06-18 13:57:42 +0300

    added the 'add_two_numbers' program with gitignore

---
+++ add_two_numbers/add_two_numbers/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Enter your two numbers!!!!!");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Get your sum!!!!");
+Console.WriteLine(n + m);
